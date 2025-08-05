a01ce29665a5fea8b3b42b338d1735c6be4107e7
Harsh Upadhyay <upadhyayharsh435@gmail.com>
2025-08-05 21:52:43 +0530

    seekbar update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log('js starts here')
+ let currentSong=new Audio();
 
 async function getSongs() {
     let a = await fetch("http://127.0.0.1:5500/songs/")
@@ -17,12 +18,22 @@ async function getSongs() {
    
 
 }
+const playMusic=(track)=>{
+    // console.log("/songs/"+track);
+    // let audio=new Audio("/songs/"+track)
+     currentSong.src="/songs/"+track
+    currentSong.play();
+    play.src="img/pause.svg"
+    document.querySelector(".songinfo").innerHTML=track
+     document.querySelector(".songtime").innerHTML="00:00 / 00:00"
+}
 async function main() {
+   
     // get the list of all songs
     let songs= await getSongs();
-    console.log(songs)
+    // console.log(songs)  song 
    let songUL= document.querySelector(".songList").getElementsByTagName("ul")[0]
-   console.log(songUL)
+//    console.log(songUL) 
    for(const song of songs){
     songUL.innerHTML=songUL.innerHTML+ ` <li>
                                 <img class="invert" src="img/music.svg" alt="">
@@ -36,13 +47,33 @@ async function main() {
                                 </div>  
                             </li>`;
    }
-    // play the first song
-    var audio=new Audio(songs[0]);
-    audio.play();
-    audio.addEventListener("loadeddata",()=>{
-        let duration=audio.duration;
-        console.log(duration);
+    // Attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click",element=>{
+        console.log(e.querySelector(".info").firstElementChild.innerHTML)
+        playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+      
     })
+
+    //attach an event listener to play , next and previous
+    play.addEventListener("click",()=>{
+        if(currentSong.paused){
+            currentSong.play();
+            play.src="img/pause.svg";
+        }
+        else{
+            currentSong.pause();
+            play.src="img/play.svg";
+        }
+
+    })
+    // listen for time update event
+    currentSong.addEventListener("timeupdate",()=>{
+        console.log(a.currentTime,a.duration)
+
+    })
+    
 
 }
 main(); 
